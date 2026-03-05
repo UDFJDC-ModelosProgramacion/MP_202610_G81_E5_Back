a1f4075d2500a8451c9c227d23b5f06fa9a13ee9
Zyrucs <danielmateomartinezgil@gmail.com>
2026-03-04 21:48:49 -0500

    Event and Shelter entities created

---
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/ShelterEntity.java
@@ -0,0 +1,23 @@
+package co.edu.udistrital.mdp.pets.entities;
+import jakarta.persistence.*;
+import uk.co.jemos.podam.common.PodamExclude;
+import lombok.Data;
+
+@Data
+@Entity
+
+public class ShelterEntity extends BaseEntity {
+    private String shelterName;
+    private String city;
+    private String location;
+    @ManyToOne 
+    @JoinColumn(name = "event_id")
+    @PodamExclude
+    private EventEntity event;
+
+   /*  @ManyToOne
+    @JoinColumn(name = "vet_id")
+    @PodamExclude
+    private VetEntity vet; */
+}
+ 
