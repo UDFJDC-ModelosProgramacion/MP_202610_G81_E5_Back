7c3508136161fde73dfaf9c3a8da719f77573f99
Zyrucs <118613645+Zyrucs@users.noreply.github.com>
2026-03-08 23:31:55 -0500

    Remove unused import in ShelterRepository
    Removed unused import for AdoptionProcessEntity.

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterRepository.java
@@ -1,7 +1,6 @@
 package co.edu.udistrital.mdp.pets.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.udistrital.mdp.pets.entities.AdoptionProcessEntity;
+import co.edu.udistrital.mdp.pets.entities.ShelterEntity;
 
 public interface ShelterRepository extends JpaRepository<ShelterEntity, Long> {
 
