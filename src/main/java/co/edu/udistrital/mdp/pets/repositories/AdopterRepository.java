152b6702e6a248f66390381915a2829b99b10e85
Zyrucs <118613645+Zyrucs@users.noreply.github.com>
2026-03-08 23:32:41 -0500

    Remove unused import in AdopterRepository
    Removed unused import for AdoptionProcessEntity.

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/AdopterRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/AdopterRepository.java
@@ -1,7 +1,6 @@
 package co.edu.udistrital.mdp.pets.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import co.edu.udistrital.mdp.pets.entities.AdoptionProcessEntity;
+import co.edu.udistrital.mdp.pets.entities.AdopterEntity;
 
 public interface AdopterRepository extends JpaRepository<AdopterEntity, Long> {
 
