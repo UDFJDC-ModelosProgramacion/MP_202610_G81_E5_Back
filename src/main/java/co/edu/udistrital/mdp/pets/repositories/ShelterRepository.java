0e0849940dc3ee56ef8ba9111db5b4bd0e971886
Zyrucs <118613645+Zyrucs@users.noreply.github.com>
2026-03-16 08:19:15 -0500

    Add @Repository annotation to ShelterRepository

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterRepository.java
@@ -1,7 +1,7 @@
 package co.edu.udistrital.mdp.pets.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import co.edu.udistrital.mdp.pets.entities.ShelterEntity;
-
+@Repository
 public interface ShelterRepository extends JpaRepository<ShelterEntity, Long> {
 
 }
