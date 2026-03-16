6ede95a4b365da182cefde17dad337081ffba921
SantiagoRincon43 <josrincona@udistrital.edu.co>
2026-03-15 19:38:42 -0500

    Update VeterinarianRepository.java
    Validar licencia unica

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/VeterinarianRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/VeterinarianRepository.java
@@ -1,8 +1,9 @@
 package co.edu.udistrital.mdp.pets.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import co.edu.udistrital.mdp.pets.entities.VeterinarianEntity;
 
 public interface VeterinarianRepository extends JpaRepository<VeterinarianEntity, Long> {
 
+    boolean existsByLicenseNumber(String licenseNumber);
 }
