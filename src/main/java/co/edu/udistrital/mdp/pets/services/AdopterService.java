8c7778daef9af7ae74b160bcc4b2b4a483e621dd
SantiagoRincon43 <josrincona@udistrital.edu.co>
2026-03-15 19:47:18 -0500

    Create AdopterService.java
    Implementacion de reglas de negocio de adopter

---
+++ src/main/java/co/edu/udistrital/mdp/pets/services/AdopterService.java
@@ -0,0 +1,50 @@
+package co.edu.udistrital.mdp.pets.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.udistrital.mdp.pets.entities.AdopterEntity;
+import co.edu.udistrital.mdp.pets.repositories.AdopterRepository;
+
+import java.util.Optional;
+
+@Service
+public class AdopterService {
+
+    @Autowired
+    private AdopterRepository adopterRepository;
+
+
+    public AdopterEntity createAdopter(AdopterEntity adopter){
+        return adopterRepository.save(adopter);
+    }
+
+
+    public AdopterEntity updateAdopter(Long id, AdopterEntity adopter) throws Exception{
+
+        Optional<AdopterEntity> adop = adopterRepository.findById(id);
+
+        if(adop.isEmpty()){
+            throw new Exception("Adoptante no encontrado");
+        }
+
+        adopter.setId(id);
+        return adopterRepository.save(adopter);
+    }
+  
+
+    public void deleteAdopter(Long id) throws Exception{
+
+        Optional<AdopterEntity> adop = adopterRepository.findById(id);
+
+        if(adop.isEmpty()){
+            throw new Exception("Adoptante no existe");
+        }
+
+        if(!adop.get().getAdoptionRequest().isEmpty()){
+            throw new Exception("No se puede eliminar adoptante con solicitudes");
+        }
+
+        adopterRepository.deleteById(id);
+    }
+}
