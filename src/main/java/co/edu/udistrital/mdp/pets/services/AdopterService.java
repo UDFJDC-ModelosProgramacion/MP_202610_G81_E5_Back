277385328481d55e7a439c31580cf53aff295c4d
SantiagoRincon43 <josrincona@udistrital.edu.co>
2026-03-16 19:56:14 -0500

    Update AdopterService.java

--- src/main/java/co/edu/udistrital/mdp/pets/services/AdopterService.java
+++ src/main/java/co/edu/udistrital/mdp/pets/services/AdopterService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import co.edu.udistrital.mdp.pets.entities.AdopterEntity;
 import co.edu.udistrital.mdp.pets.repositories.AdopterRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,24 +16,37 @@ public class AdopterService {
     private AdopterRepository adopterRepository;
 
 
-    public AdopterEntity createAdopter(AdopterEntity adopter){
-        return adopterRepository.save(adopter);
+    public List<AdopterEntity> getAdopters(){
+        return adopterRepository.findAll();
     }
 
-    
-
-    public AdopterEntity updateAdopter(Long id, AdopterEntity adopter) throws Exception{
-
+    public AdopterEntity getAdopter(Long id) throws Exception{
         Optional<AdopterEntity> adop = adopterRepository.findById(id);
 
         if(adop.isEmpty()){
             throw new Exception("Adoptante no encontrado");
         }
 
+        return adop.get();
+    }
+
+
+    public AdopterEntity createAdopter(AdopterEntity adopter){
+        return adopterRepository.save(adopter);
+    }
+
+    public AdopterEntity updateAdopter(Long id, AdopterEntity adopter) throws Exception{
+
+        Optional<AdopterEntity> existing = adopterRepository.findById(id);
+
+        if(existing.isEmpty()){
+            throw new Exception("Adoptante no encontrado");
+        }
+
         adopter.setId(id);
         return adopterRepository.save(adopter);
     }
-  
+
 
     public void deleteAdopter(Long id) throws Exception{
 
