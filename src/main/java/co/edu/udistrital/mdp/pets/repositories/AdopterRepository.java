3f3b94cfa5853aff99245d0d5e5d287ef3a4715b
SantiagoRincon43 <josrincona@udistrital.edu.co>
2026-05-19 22:29:22 -0500

    Update AdopterRepository.java
    verifica email

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/AdopterRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/AdopterRepository.java
@@ -2,8 +2,9 @@ package co.edu.udistrital.mdp.pets.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import co.edu.udistrital.mdp.pets.entities.AdopterEntity;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface AdopterRepository extends JpaRepository<AdopterEntity, Long> {
-
+    Optional<AdopterEntity> findByEmail(String email);
 }
